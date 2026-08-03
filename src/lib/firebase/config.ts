21f6602f0183faa1ad110e6dc4221533db86c19d
MarioMunPeq <snakeyes271101@gmail.com>
2026-08-03 19:44:52 +0200

    feat(firebase): log Firebase environment variables for debugging

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -32,6 +32,14 @@ function loadEnv(): Record<string, string | undefined> {
 
 const env = loadEnv();
 
+console.log("Firebase env", {
+  apiKey: env.VITE_FIREBASE_API_KEY,
+  authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: env.VITE_FIREBASE_PROJECT_ID,
+  appId: env.VITE_FIREBASE_APP_ID,
+  messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+});
+
 const firebaseConfig: FirebaseConfig = {
   apiKey: env.VITE_FIREBASE_API_KEY ?? "",
   authDomain: env.VITE_FIREBASE_AUTH_DOMAIN ?? "",
